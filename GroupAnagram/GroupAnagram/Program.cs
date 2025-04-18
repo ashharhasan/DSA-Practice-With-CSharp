0284db7f6ad2e27df7c7193343266e62598a98b1
Muhammad Ashhar Hasan <hasan_ashhar@hotmail.com>
2025-04-18 23:16:09 +0200

    Merging all projects to single repo

---
+++ GroupAnagram/GroupAnagram/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+
+string[] input = { "act", "pots", "tops", "cat", "stop", "hat" };
+        var grouped = GroupAnagrams(input);
+
+        foreach (var group in grouped)
+        {
+            Console.WriteLine($"[{string.Join(", ", group)}]");
+        }
+
+List<List<string>> GroupAnagrams(string[] strs) {
+    Dictionary<string, List<string>> d1 = new Dictionary<string, List<string>>();
+
+    foreach(var str in strs)
+    {
+        string hkey= GetFreqMap(str);
+        if(!d1.ContainsKey(hkey))
+        {
+            d1.Add(hkey,new List<string>());
+        }
+        d1[hkey].Add(str);
+    }
+    return d1.Values.ToList();
+}
+
+string GetFreqMap(string str)
+{
+    int[] freq = new int[26];
+    foreach(char c in str)
+    {
+        freq[c - 'a']++;
+    }
+    return String.Join('#',freq);
+}
